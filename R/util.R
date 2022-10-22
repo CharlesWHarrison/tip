101d37f82db9a649b0f021116a3f8ce8ec9995e8
research-knight <70669414+research-knight@users.noreply.github.com>
2022-10-21 22:24:07 -0400

    Edited the titles of the automatic matrix inversion functions and then added a make_invertible_det function that inverts a matrix based on the determinant instead of the eigenvalues.

--- R/util.R
+++ R/util.R
@@ -1,4 +1,4 @@
-#' @title Automatic matrix inversion
+#' @title Automatic matrix inversion checking via an eigenvalue criterion
 #' @param .matrix The matrix that may or may not be invertible.
 #' @param .tolerance If necessary, a small value that is ITERATIVELY added to each diagonal element of a matrix until the matrix is invertible.
 #' @noRd
@@ -21,6 +21,28 @@ make_invertible <- function(.matrix, .tolerance = 0.01){
   return(.matrix)
   # make_invertible(.matrix = matrix(data = 1, nrow = 3, ncol = 3),
   #                 .tolerance = 0.01)
+}
+
+#' @title Automatic matrix inversion checking via a determinant criterion
+#' @param .matrix Square matrix. The matrix that may or may not be invertible.
+#' @param .step_size Positive numeric. If necessary, a small value that is ITERATIVELY added to each diagonal element of a matrix until the matrix is invertible.
+#' @noRd
+make_invertible_det <- function(.matrix, .step_size = 0.01){
+  # --- A function used to make a matrix invertible by
+  # adding a small number to the matrix's diagonal.
+  # If the matrix is invertible, then return the matrix. ---
+  if(det(.matrix)< 0.001){
+    .bias_size <- 0.001
+    num_rows_cols <- dim(.matrix)[1]
+    .temp <- .matrix + diag(num_rows_cols)*.bias_size
+    if(det(.temp) > 0.001){.matrix = .temp}
+    while(det(.temp) <= 0.001){
+      .temp = .matrix + diag(num_rows_cols)*.bias_size
+      .bias_size = .bias_size + .step_size
+    }
+    .matrix <- .temp
+  }
+  return(.matrix)
 }
 
 #' @title Partition an Undirected Graph
