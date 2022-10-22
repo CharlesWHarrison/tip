9bd837ca6ec0dd51f4a2c175142830b0dfe3c28d
research-knight <70669414+research-knight@users.noreply.github.com>
2022-10-21 23:06:34 -0400

    Changed the ".tolerance" variable name to ".step_size".

--- R/util.R
+++ R/util.R
@@ -1,26 +1,26 @@
 #' @title Automatic matrix inversion checking via an eigenvalue criterion
-#' @param .matrix The matrix that may or may not be invertible.
-#' @param .tolerance If necessary, a small value that is ITERATIVELY added to each diagonal element of a matrix until the matrix is invertible.
+#' @param .matrix Matrix. The matrix that may or may not be invertible.
+#' @param .step_size Numeric. If necessary, a small value that is ITERATIVELY added to each diagonal element of a matrix until the matrix is invertible.
 #' @noRd
-make_invertible <- function(.matrix, .tolerance = 0.01){
+make_invertible <- function(.matrix, .step_size = 0.01){
   # --- A function used to make a matrix invertible by
   # adding a small number to the matrix's diagonal.
   # If the matrix is invertible, then return the matrix. ---
-
+  .bias_size <- .step_size
   # Compute the number of columns
   .dimension <- dim(.matrix)[2]
   if(any(eigen(.matrix)$values <= 0.0)){
-    .temp <- .matrix + diag(.dimension)*.tolerance
-    if(all(eigen(.matrix)$values > .tolerance)){return(.temp)}
-    while(any(eigen(.matrix)$values < 0.0)){
-      .temp = .matrix + diag(.dimension)*.tolerance
-      .tolerance = .tolerance + .tolerance
+    .temp <- .matrix + diag(.dimension)*.bias_size
+    if(all(eigen(.matrix)$values > .step_size)){return(.temp)}
+    while(any(eigen(.temp)$values < 0.0)){
+      .temp = .matrix + diag(.dimension)*.bias_size
+      .bias_size = .bias_size + .step_size
     }
     .matrix <- .temp
   }
   return(.matrix)
   # make_invertible(.matrix = matrix(data = 1, nrow = 3, ncol = 3),
-  #                 .tolerance = 0.01)
+  #                 .step_size = 0.01)
 }
 
 #' @title Automatic matrix inversion checking via a determinant criterion
@@ -32,7 +32,7 @@ make_invertible_det <- function(.matrix, .step_size = 0.01){
   # adding a small number to the matrix's diagonal.
   # If the matrix is invertible, then return the matrix. ---
   if(det(.matrix)< 0.001){
-    .bias_size <- 0.001
+    .bias_size <- .step_size
     num_rows_cols <- dim(.matrix)[1]
     .temp <- .matrix + diag(num_rows_cols)*.bias_size
     if(det(.temp) > 0.001){.matrix = .temp}
